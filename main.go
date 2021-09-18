4939c5fc5e2e0eb93f6823fb80ed9520ef61f9ca
pseudobear <2002michaelli@gmail.com>
2021-09-18 14:22:03 -0400

    Read support for bottles

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
     "github.com/gorilla/mux"
 )
 type Bottle struct {
-    Id int `json:"Id"`
+    Id string `json:"Id"`
     Title string `json:"Title"`
     Tag string `json:"Tag"`
     Content string `json:"Content"`
@@ -26,15 +26,30 @@ func homePage(w http.ResponseWriter, r *http.Request){
 
 func returnAllBottles(w http.ResponseWriter, r *http.Request){
     fmt.Println("Endpoint Hit: returnAllArticles")
+    w.Header().Set("Content-Type", "application/json")
     json.NewEncoder(w).Encode(bottles)
+}
+
+func returnBottleById(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("Endpoint Hit: returnBottleById")
+    w.Header().Set("Content-Type", "application/json")
+    params := mux.Vars(r)
+
+    for _, item := range bottles {
+        if item.Id == params["id"] {
+            json.NewEncoder(w).Encode(item)
+            return
+        }
+    }
 }
 
 func handleRequests() {
     // creates a new instance of a mux router
     myRouter := mux.NewRouter().StrictSlash(true)
     // replace http.HandleFunc with myRouter.HandleFunc
-    myRouter.HandleFunc("/", homePage)
-    myRouter.HandleFunc("/all", returnAllBottles)
+    myRouter.HandleFunc("/", homePage).Methods("GET");
+    myRouter.HandleFunc("/all", returnAllBottles).Methods("GET")
+    myRouter.HandleFunc("/byId/{id}", returnBottleById).Methods("GET")
     // finally, instead of passing in nil, we want
     // to pass in our newly created router as the second
     // argument
@@ -44,9 +59,8 @@ func handleRequests() {
 func main() {
     fmt.Println("Rest API v2.0 - Mux Routers")
     bottles = []Bottle{
-      Bottle{Id: 0, Title: "example", Tag: "misc", Content: "hello world"},
-      Bottle{Id: 1, Title: "example", Tag: "misc", Content: "hello world!"},
+        Bottle{Id: "0", Title: "example", Tag: "misc", Content: "hello world"},
+        Bottle{Id: "1", Title: "example", Tag: "misc", Content: "hello world!"},
     }
     handleRequests()
-
 }
