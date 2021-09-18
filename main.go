67a806ae7f2c9052947ccacb5d5b564e6e34d485
Jason Zhou <65838712+ZhouJas@users.noreply.github.com>
2021-09-18 15:34:39 -0400

    returnRandomBottle

--- main.go
+++ main.go
@@ -1,17 +1,20 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
-    "encoding/json"
-    "github.com/gorilla/mux"
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
+
 type Bottle struct {
-    Id string `json:"Id"`
-    Title string `json:"Title"`
-    Tag string `json:"Tag"`
-    Content string `json:"Content"`
+	Id      string   `json:"Id"`
+	Title   string   `json:"Title"`
+	Tag     []string `json:"Tag"`
+	Content string   `json:"Content"`
 }
 
 // let's declare a global Articles array
@@ -19,64 +22,99 @@ type Bottle struct {
 // to simulate a database
 var bottles []Bottle
 
-func homePage(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Welcome to the HomePage!")
-    fmt.Println("Endpoint Hit: homePage")
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
 }
 
-func returnAllBottles(w http.ResponseWriter, r *http.Request){
-    fmt.Println("Endpoint Hit: returnAllArticles")
-    w.Header().Set("Content-Type", "application/json")
-    json.NewEncoder(w).Encode(bottles)
+func returnAllBottles(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllArticles")
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(bottles)
 }
 
 func returnBottleById(w http.ResponseWriter, r *http.Request) {
-    fmt.Println("Endpoint Hit: returnBottleById")
-    w.Header().Set("Content-Type", "application/json")
-    params := mux.Vars(r)
+	fmt.Println("Endpoint Hit: returnBottleById")
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
 
-    for _, item := range bottles {
-        if item.Id == params["id"] {
-            json.NewEncoder(w).Encode(item)
-            return
-        }
-    }
+	json.NewEncoder(w).Encode(returnSpecificBottle(params["id"]))
+}
+
+func returnRandomBottle(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnRandomBottle")
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	matchingBottles := []Bottle{}
+	for _, item := range bottles {
+		if stringExists(params["tag"], item.Tag) {
+			matchingBottles = append(matchingBottles, item)
+		}
+	}
+	min := 0
+	max := len(matchingBottles)
+	if max == min {
+		json.NewEncoder(w).Encode(returnSpecificBottle("0"))
+		return
+	}
+
+	json.NewEncoder(w).Encode(matchingBottles[rand.Intn(max-min+1)])
+
+}
+
+func stringExists(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
+
+func returnSpecificBottle(id string) Bottle {
+	for _, item := range bottles {
+		if item.Id == id {
+			return item
+		}
+	}
+	return Bottle{}
 }
 
 func deleteBottleById(w http.ResponseWriter, r *http.Request) {
-    fmt.Println("Endpoint Hit: deleteBottleById")
-    w.Header().Set("Content-Type", "application/json")
-    params := mux.Vars(r)
+	fmt.Println("Endpoint Hit: deleteBottleById")
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
 
-    for index, item := range bottles {
-        if item.Id == params["id"] {
-            json.NewEncoder(w).Encode(item)
-            bottles = append(bottles[:index], bottles[index+1:]...)
-            return
-        }
-    }
+	for index, item := range bottles {
+		if item.Id == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			bottles = append(bottles[:index], bottles[index+1:]...)
+			return
+		}
+	}
 }
 
-
 func handleRequests() {
-    // creates a new instance of a mux router
-    myRouter := mux.NewRouter().StrictSlash(true)
-    // replace http.HandleFunc with myRouter.HandleFunc
-    myRouter.HandleFunc("/", homePage).Methods("GET");
-    myRouter.HandleFunc("bottles/all", returnAllBottles).Methods("GET")
-    myRouter.HandleFunc("bottles/{id}", returnBottleById).Methods("GET")
-    myRouter.HandleFunc("bottles/{id}", deleteBottleById).Methods("DELETE")
-    // finally, instead of passing in nil, we want
-    // to pass in our newly created router as the second
-    // argument
-    log.Fatal(http.ListenAndServe(":10000", myRouter))
+	// creates a new instance of a mux router
+	myRouter := mux.NewRouter().StrictSlash(true)
+	// replace http.HandleFunc with myRouter.HandleFunc
+	myRouter.HandleFunc("/", homePage).Methods("GET")
+	myRouter.HandleFunc("bottles/all", returnAllBottles).Methods("GET")
+	myRouter.HandleFunc("bottles/{id}", returnBottleById).Methods("GET")
+	myRouter.HandleFunc("bottles/{tag}", returnRandomBottle).Methods("GET")
+	myRouter.HandleFunc("bottles/{id}", deleteBottleById).Methods("DELETE")
+	// finally, instead of passing in nil, we want
+	// to pass in our newly created router as the second
+	// argument
+	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
 
 func main() {
-    fmt.Println("Rest API v2.0 - Mux Routers")
-    bottles = []Bottle{
-        Bottle{Id: "0", Title: "example", Tag: "misc", Content: "hello world"},
-        Bottle{Id: "1", Title: "example", Tag: "misc", Content: "hello world!"},
-    }
-    handleRequests()
+	fmt.Println("Rest API v2.0 - Mux Routers")
+	bottles = []Bottle{
+		Bottle{Id: "0", Title: "example", Tag: []string{"10"}, Content: "hello world"},
+		Bottle{Id: "1", Title: "example", Tag: []string{"10", "20"}, Content: "hello world!"},
+	}
+	handleRequests()
 }
